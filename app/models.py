d33664d4694fe2fb114c7fc9aa5ba040ab084b39
areej <areej.negm@mail.utoronto.ca>
2023-02-11 00:51:29 -0500

    userInfo model fixed

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from exts import db
 
 class User(db.Model):
+    __tablename__ = "user_table"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.Text(), nullable=False)
@@ -11,9 +12,15 @@ class User(db.Model):
         db.session.commit()
 
 class UserInfo(db.Model):
+    __tablename__ = "userInfo_table"
     age = db.Column(db.Integer, nullable=False)
     mobileApps = db.Column(db.Integer, nullable=True)
     internet = db.Column(db.Integer, nullable=True)
     socialMedia = db.Column(db.Integer, nullable=True)
     banking = db.Column(db.Integer, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_table.id"))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 

2386a1cb12dc20ba84f2330d264e6e4d5dc04939
areej <areej.negm@mail.utoronto.ca>
2023-02-11 01:55:02 -0500

    git ignore

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS, cross_origin
 from flask_session import Session
+
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
--- app/models.py
+++ app/models.py
@@ -2,10 +2,10 @@ from exts import db
 
 class User(db.Model):
     __tablename__ = "user_table"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), primary_key=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
     userinfo = db.relationship("UserInfo", back_populates="user")
+    age = db.Column(db.Integer, nullable=False)
 
     def save(self):
         db.session.add(self)
@@ -13,12 +13,12 @@ class User(db.Model):
 
 class UserInfo(db.Model):
     __tablename__ = "userInfo_table"
-    age = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, nullable=False)
     mobileApps = db.Column(db.Integer, nullable=True)
     internet = db.Column(db.Integer, nullable=True)
     socialMedia = db.Column(db.Integer, nullable=True)
     banking = db.Column(db.Integer, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user_table.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user_table.email"))
 
     def save(self):
         db.session.add(self)

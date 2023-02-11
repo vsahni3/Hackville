2d6ca0b2d224a85046ab227a43767d916b21babe
Varun Sahni <varunsahni3504@gmail.com>
2023-02-11 08:46:12 -0500

    finish cohere

--- app/main.py
+++ app/main.py
@@ -1,20 +1,125 @@
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS, cross_origin
 from flask_session import Session
+from exts import db
+from ml import reply
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 Session(app)
-cors = CORS(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    email = db.Column(db.String(80), primary_key=True, nullable=False)
+    age = db.Column(db.String(80), nullable=False)
+    hardware = db.Column(db.String(80), nullable=True)
+    social_media = db.Column(db.String(80), nullable=True)
+    banking = db.Column(db.String(80), nullable=True)
+    extra_info = db.Column(db.String(300), nullable=True)
+
+    def __init__(self, email, age, hardware, social_media, banking, extra_info):
+        self.email = email
+        self.age = age
+        self.hardware = hardware
+        self.social_media = social_media
+        self.banking = banking
+        self.extra_info = extra_info
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+# with app.app_context():
+#     db.create_all()
 @app.route('/login/', methods=['POST'])
 def login():
     email = request.get_json()['email']
-    # check if email exists
-    # if not, create a table
+    session['email'] = email
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        new_user = User(email, '', '', '', '', '')
+        db.session.add(new_user)
+        db.session.commit()
+
+    return jsonify({
+        'response': 'Done'
+    })
+
 
 @app.route('/inputs/', methods=['POST'])
 def inputs():
-    # get info
-    # store info
-    pass
+    age = request.get_json()['age']
+    hardware = request.get_json()['hardware']
+    social_media = request.get_json()['social_media']
+    banking = request.get_json()['banking']
+    extra_info = request.get_json()['extra_info']
+    
+    email = session.get('email')
+    if not email:
+        response = {'status': 'not logged in'}
+        return jsonify(response)
+
+    user = User.query.filter_by(email=email).first()
+    user.age = age
+    user.hardware = hardware
+    user.social_media = social_media
+    user.banking = banking
+    user.extra_info = extra_info
+    db.session.commit()
+
+    return jsonify({
+        'response': 'Done'
+    })
+
+
+@app.route('/msg/', methods=['POST'])
+def msg():
+    text = request.get_json()['text']
+    email = session.get('email')
+    if not email:
+        response = {'status': 'not logged in'}
+        return jsonify(response)
+
+    user = User.query.filter_by(email=email).first()
+    prompt = f"""
+    I am {user.age} years old. I will add my comfort levels as a number, with using the 
+    following fields below:
+    Banking: {user.banking}
+    Hardware: {user.hardware}
+    Social Media: {user.social_media}
+    Here is some additional info about how comfortable I am with using technology:
+    {user.extra_info}
+
+    Given all this info, respond to the following question about help with technology asked by the above user
+    {text}
+    """
+    response = reply(prompt)
+    return jsonify({
+        'response': response
+    })
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+
+
+
+
+
+    
+ 
+
+    
+    
+
+    
+    
+
+
 
--- app/ml.py
+++ app/ml.py
@@ -1,9 +1,9 @@
 import cohere
 
-co = cohere.client('ZG1hp4UsOagPS7V8vOiSxkGMljolDMPi96KAvboq')
+co = cohere.Client('ZG1hp4UsOagPS7V8vOiSxkGMljolDMPi96KAvboq')
 
 def reply(prompt):
-    
+
     response = co.generate(
     model='command-xlarge-nightly',
     prompt=prompt,
@@ -13,3 +13,17 @@ def reply(prompt):
     return_likelihoods='NONE',
     truncate='START')
     return response.generations[0].text
+
+prompt = f"""
+I am 71 years old. I will add my comfort levels as a number, with using the 
+following fields below:
+Banking: 7
+Hardware: 4
+Social Media: 7
+Here is some additional info about how comfortable I am with using technology:
+I struggle a lot with uploading pictures to social media specifically.
+
+Given all this info, respond to the following question about help with technology asked by the above user
+How do I send a friend request on Facebook?
+"""
+# print(reply(prompt))
